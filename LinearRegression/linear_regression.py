ff860675743291ebe7a6ae7db273ff2b63c72ee8
Sakchhi Srivastava <sakchhi.sri@gmail.com>
2020-01-27 22:33:30 +0530

    Multiple Linear Regression -- fit -- predict -- intercept

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -4,32 +4,49 @@ import numpy as np
 class LinearRegression:
 
     def __init__(self, fit_intercept=True):
-        self.coefs_ = np.nan
-        self.intercept_ = np.nan
+        self.coefs_ = None
+        self.intercept_ = None
+        self.fit_intercept = fit_intercept
 
+    def add_constant(self, X):
+        return np.c_[X, np.ones(X.shape[0])]
 
+    def fit(self, X, y):
+        """
+        Returns coefficients b1, b0 of an equation of the form y = b1.x + b0
+        which is the least squares solution providing the line of best fit
+        :param x: numpy array, representing independent features
+        :param y: numpy array, representing dependent feature
+        :return:
+        """
+        if self.fit_intercept:
+            X = self.add_constant(X)
+        b = (np.linalg.inv(X.T.dot(X)).dot(X.T)).dot(y)
+        self.coefs_ = b
+        return self
 
-
-def fit(x, y):
-    """
-    Returns coefficients b1, b0 of an equation of the form y = b1.x + b0
-    which is the least squares solution providing the line of best fit
-    :param x: numpy array, representing independent features
-    :param y: numpy array, representing dependent feature
-    :return:
-    """
-    print((x - x.mean()).shape)
-    Sxy = np.matmul((x - x.mean(axis=0)), (y - y.mean(axis=0)))
-    Sxx = np.square((x - np.mean(x))).sum()
-
-    b1 = Sxy/Sxx
-    b0 = np.mean(y) - b1*np.mean(x)
-    return b1, b0
+    def predict(self, test):
+        """
+        Return predictions for fitted model
+        :param test:
+        :return:
+        """
+        if not all(self.coefs_):
+            raise ValueError("Fit the model before predicting")
+        predictions = test.dot(self.coefs_[:-1]) + self.coefs_[-1]
+        return predictions
 
 
 if __name__ == '__main__':
     x = np.random.rand(100, 5)
     actual_coeffs = np.array([4.5, 5.2, 3.7, 9.1, 6.8])
-    y = np.array((x*actual_coeffs).sum(axis=1) + 5).reshape(-1, 1)
+    y = np.array((x * actual_coeffs).sum(axis=1) + 5).reshape(-1, 1)
     print(y.shape, x.shape)
-    print(fit(x, y))
+    linear_reg = LinearRegression()
+    linear_reg.fit(x, y)
+    x_test = np.random.rand(10, 5)
+    y_test = np.array((x_test * actual_coeffs).sum(axis=1) + 5).reshape(-1, 1)
+    print(linear_reg.coefs_)
+    y_pred = linear_reg.predict(x_test)
+    err = (abs(y_test - y_pred)/y_test).mean()
+    print(err)
