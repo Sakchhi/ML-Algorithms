461e6358942da250c036d3818dcd94c27cc20d48
Sakchhi Srivastava <sakchhi.sri@gmail.com>
2020-01-27 21:45:07 +0530

    Modifying for Multiple Linear Regression

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 
-def get_coeffs(x, y):
+class LinearRegression:
+
+    def __init__(self, fit_intercept=True):
+        self.coefs_ = np.nan
+        self.intercept_ = np.nan
+
+
+
+
+def fit(x, y):
     """
     Returns coefficients b1, b0 of an equation of the form y = b1.x + b0
     which is the least squares solution providing the line of best fit
@@ -9,7 +18,8 @@ def get_coeffs(x, y):
     :param y: numpy array, representing dependent feature
     :return:
     """
-    Sxy = np.matmul((x - np.mean(x)), (y - np.mean(y)))
+    print((x - x.mean()).shape)
+    Sxy = np.matmul((x - x.mean(axis=0)), (y - y.mean(axis=0)))
     Sxx = np.square((x - np.mean(x))).sum()
 
     b1 = Sxy/Sxx
@@ -18,6 +28,8 @@ def get_coeffs(x, y):
 
 
 if __name__ == '__main__':
-    x = np.random.randn(100)
-    y = [5*i + 4 for i in x]
-    print(get_coeffs(x, y))
+    x = np.random.rand(100, 5)
+    actual_coeffs = np.array([4.5, 5.2, 3.7, 9.1, 6.8])
+    y = np.array((x*actual_coeffs).sum(axis=1) + 5).reshape(-1, 1)
+    print(y.shape, x.shape)
+    print(fit(x, y))
